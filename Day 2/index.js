59b85522c654e10918673c8f3820cb666593984c
jospen-kah <ngumkahjospen@gmail.com>
2024-11-18 11:26:14 +0100

    moment

--- Day 2/index.js
+++ Day 2/index.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const app = express();
+const members = require('./members')
+const moment = require('moment')
+
 const path = require('path')
 
 const Port = process.env.PORT || 3000;
+
+const logger = (req, res, next)=>{
+    console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}:${moment().format()}`);
+    next();
+}
+
+//init
+app.use(logger);
+
+app.use('/api/members', (req, res) =>{
+    res.json(members);
+});
 
 // set static folder
 app.use(express.static(path.join(__dirname, 'public')));
---
+++ Day 2/members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1, 
+        name: 'Jospen',
+        email: 'jospen@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 2, 
+        name: 'Shabib',
+        email: 'Shabib@gmail.com',
+        status: 'inactive'
+    },
+    {
+        id: 3, 
+        name: 'Bob Marley',
+        email: 'bob@gmail.com',
+        status: 'active'
+    }
+]
+
+module.exports = members;
