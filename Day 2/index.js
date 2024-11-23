6b0e061f967139a9810ce2cf72eeafc6b6234e11
jospen-kah <ngumkahjospen@gmail.com>
2024-11-23 06:08:03 +0100

    crude

--- Day 2/index.js
+++ Day 2/index.js
@@ -1,22 +1,31 @@
 const express = require('express');
 const app = express();
-const router = require('./routes/api/courses')
-
+const exphbs = require('express-handlebars');
+const router = require('./routes/api/courses');
+const path = require('path');
+const courses = require('./Courses')
 // const logger = require('./middleware/logger')
 
+// // set static folder
+// app.use(express.static(path.join(__dirname, 'public')));
 
-const path = require('path')
 
 const Port = process.env.PORT || 3000;
 
-
+// Use .engine
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' })); 
+app.set('view engine', 'handlebars');
+app.get('/' , (req, res) => res.render('index', {
+    title: 'My Courses App',
+    courses
+}))
 
 //Body Parser Middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: false}))
 
 // set static folder
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/courses', router)
 
--- Day 2/routes/api/courses.js
+++ Day 2/routes/api/courses.js
@@ -18,27 +18,27 @@ router.get('/:id', (req, res) => {
     }
 
     else {
-        res.status(400).json({message : `No course with the id of ${req.params.id}`})
+        res.status(400).json({ message: `No course with the id of ${req.params.id}` })
     }
 
 })
 
 
 //Post Courses
-router.post('/', (req, res)=>{
-    const {course_name,course_code}=req.body
-    if(!course_name || !course_code){
-        res.status(400).json({message:'Fields required '})
+router.post('/', (req, res) => {
+    const { course_name, course_code } = req.body
+    if (!course_name || !course_code) {
+        res.status(400).json({ message: 'Fields required ' })
     }
-const newCourse = {
-    id:uuid.v4(),
-    course_name,
-    course_code
-}
+    const newCourse = {
+        id: uuid.v4(),
+        course_name,
+        course_code
+    }
 
-   courses.push(newCourse)
-   res.json(courses)
-
+    // courses.push(newCourse)
+    //    res.json(courses)
+    res.redirect('/')
 })
 
 //Update Course
@@ -47,29 +47,29 @@ router.put('/:id', (req, res) => {
     if (found) {
         const upDCourse = req.body;
         courses.forEach(course => {
-            if( course.id === parseInt(req.params.id)){
+            if (course.id === parseInt(req.params.id)) {
                 course.course_name = upDCourse.course_name ? upDCourse.course_name : course.course_name;
                 course.course_code = upDCourse.course_code ? upDCourse.course_code : course.course_code;
 
-                res.json({ msg: " Course Updated", course})
+                res.json({ msg: " Course Updated", course })
             };
         });
     }
 
     else {
-        res.status(400).json({message : `No course with the id of ${req.params.id}`})
+        res.status(400).json({ message: `No course with the id of ${req.params.id}` })
     }
 
 })
 //Delete Course
-router.delete('/:id', (req, res) =>{
+router.delete('/:id', (req, res) => {
     const found = courses.some(course => course.id === parseInt(req.params.id))
 
-    if (found){
-        res.json({msg : 'Course Deleted', courses: courses.filter(course => course.id !== parseInt(req.params.id))})
+    if (found) {
+        res.json({ msg: 'Course Deleted', courses: courses.filter(course => course.id !== parseInt(req.params.id)) })
     }
     else {
-        res.status(400).json({message : `No course with the id of ${req.params.id}`})
+        res.status(400).json({ message: `No course with the id of ${req.params.id}` })
     }
 
 })
